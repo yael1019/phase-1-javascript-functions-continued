0486b94bd4aefa79a1e4458c32e2e8ef1eaa64b7
Yael Harosh <yaelharosh01@gmail.com>
2023-02-20 15:14:43 -0500

    pass all functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (toDo = 'go to the office') {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(p = '*') {
+    return function (p1 = 'special') {
+        return `You are ${p}${p1}${p}!`;
+    }
+}
